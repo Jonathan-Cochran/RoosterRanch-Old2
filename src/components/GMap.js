e0edafb10eee8c26e5565b1b396a800363a2ea78
Jonathan Cochran <jonathan.cochran@gmail.com>
2019-04-14 19:54:47 -0400

    Added Markers to the Map

--- src/components/GMap.js
+++ src/components/GMap.js
@@ -1,12 +1,32 @@
 import React, { Component } from 'react';
-import { Map, GoogleApiWrapper } from 'google-maps-react';
+import { Map, GoogleApiWrapper, Marker, InfoWindow } from 'google-maps-react';
 
 const mapStyles = {
-  width: '100%',
+  width: '75%',
   height: '100%'
 };
 
 export class MapContainer extends Component {
+  state = {
+    showingInfoWindow: false,  //Hides or the shows the infoWindow
+    activeMarker: {},          //Shows the active marker upon click
+    selectedPlace: {}          //Shows the infoWindow to the selected place upon a marker
+  };
+  onMarkerClick = (props, marker, e) =>
+    this.setState({
+      selectedPlace: props,
+      activeMarker: marker,
+      showingInfoWindow: true
+    });
+
+  onClose = props => {
+    if (this.state.showingInfoWindow) {
+      this.setState({
+        showingInfoWindow: false,
+        activeMarker: null
+      });
+    }
+  };
   render() {
     return (
       <Map
@@ -16,8 +36,38 @@ export class MapContainer extends Component {
         initialCenter={{
          lat: 39.958065,
          lng: -82.997717
-        }}
-      />
+        }}>
+          <Marker
+            onClick={this.onMarkerClick}
+            name={'Root Insurance'}
+            position={{lat: 39.958065, lng: -82.997717}}
+          />
+          <InfoWindow
+            marker={this.state.activeMarker}
+            visible={this.state.showingInfoWindow}
+            onClose={this.onClose}
+          >
+            <div>
+              <h4>{this.state.selectedPlace.name}</h4>
+            </div>
+          </InfoWindow>
+
+          <Marker
+            onClick={this.onMarkerClick}
+            name={'SS1'}
+            position={{lat: 39.95, lng: -82.99}}
+          />
+          <InfoWindow
+            marker={this.state.activeMarker}
+            visible={this.state.showingInfoWindow}
+            onClose={this.onClose}
+          >
+            <div>
+              <h4>{this.state.selectedPlace.name}</h4>
+            </div>
+          </InfoWindow>
+
+      </Map>
     );
   }
 }
